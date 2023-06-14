517742befa2301af4b14c17aaf31b1d7255b319e
Chris Manson <chris@manson.ie>
2023-06-14 10:28:46 +0100

    update to layer-gen

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var fleck = require('fleck');
+const Blueprint = require('layer-gen/lib/models/blueprint');
 
 var dasherize = fleck.dasherize;
 var classify  = fleck.upperCamelize;
 
-module.exports = {
-  description: 'The default blueprint for ember-cli projects.',
+module.exports = class AppBlueprint extends Blueprint {
+  description = 'The default blueprint for ember-cli projects.';
 
-  locals: function(options) {
+  locals(options) {
     var entity    = options.entity;
     var rawName   = entity.name;
     var name      = dasherize(rawName);
